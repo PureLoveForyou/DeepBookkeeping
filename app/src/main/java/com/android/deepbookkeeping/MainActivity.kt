fddfd81ae1a8fa3e02f5cdacd884249dc89e9ab4
Desmond <hi.desmond.huang@gmail.com>
2025-05-16 02:10:44 +0800

    Add bottomNavigation and three fragments

--- app/src/main/java/com/android/deepbookkeeping/MainActivity.kt
+++ app/src/main/java/com/android/deepbookkeeping/MainActivity.kt
@@ -5,6 +5,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +23,22 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.navigation_view)
+        val controller: NavController = this.findNavController(R.id.fragmentContainerView)
+
+//        val configuration = AppBarConfiguration.Builder(bottomNavigationView.menu).build()
+//        NavigationUI.setupActionBarWithNavController(this, controller, configuration)
+//        NavigationUI.setupWithNavController(bottomNavigationView, controller)
+        val configuration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_report,
+                R.id.navigation_settings
+            )
+        )
+
+        NavigationUI.setupActionBarWithNavController(this, controller, configuration)
+        bottomNavigationView.setupWithNavController(controller)
     }
 }
---
+++ app/src/main/java/com/android/deepbookkeeping/ui/report/ReportViewModel.kt
@@ -0,0 +1,7 @@
+package com.android.deepbookkeeping.ui.report
+
+import androidx.lifecycle.ViewModel
+
+class ReportViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
