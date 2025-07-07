2d0b7c9745a89e96666b45d18d923d84d7c22ce1
Desmond <hi.desmond.huang@gmail.com>
2025-07-08 00:43:12 +0800

    Add repository

--- app/src/main/java/com/android/deepbookkeeping/data/repository/AccountingRepository.kt
+++ app/src/main/java/com/android/deepbookkeeping/data/repository/AccountingRepository.kt
@@ -1,4 +1,17 @@
 package com.android.deepbookkeeping.data.repository
 
-class AccountingRepository {
+import com.android.deepbookkeeping.data.local.dao.AppDao
+import com.android.deepbookkeeping.data.local.entity.Transaction
+
+class AccountingRepository(private val appDao: AppDao) {
+    suspend fun insert(transaction: Transaction) = appDao.insert(transaction)
+
+    suspend fun update(transaction: Transaction) = appDao.update(transaction)
+
+    suspend fun delete(transaction: Transaction) = appDao.delete(transaction)
+
+    fun getAllTransactions() = appDao.getAllTransactions()
+
+    fun getTotalAmount(type: Int, startTime: Long, endTime: Long) =
+        appDao.getTotalAmount(type, startTime, endTime)
 }
