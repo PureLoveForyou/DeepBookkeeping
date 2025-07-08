9d83b338fe6b95946d724345ae329592dbc53017
Desmond <hi.desmond.huang@gmail.com>
2025-07-08 20:32:27 +0800

    Change navigationBar and toolbar color

--- app/src/main/java/com/android/deepbookkeeping/MainActivity.kt
+++ app/src/main/java/com/android/deepbookkeeping/MainActivity.kt
@@ -1,17 +1,15 @@
 package com.android.deepbookkeeping
 
 import android.os.Bundle
-import android.view.SurfaceControl.Transaction
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupWithNavController
-import com.android.deepbookkeeping.ui.bottomsheet.AddTransactionDialogFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
@@ -25,24 +23,14 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
+        val toolbar = findViewById<Toolbar>(R.id.tool_bar)
+        setSupportActionBar(toolbar)
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.navigation_view)
         val controller: NavController = this.findNavController(R.id.fragmentContainerView)
 
-//        val configuration = AppBarConfiguration.Builder(bottomNavigationView.menu).build()
-//        NavigationUI.setupActionBarWithNavController(this, controller, configuration)
+        val configuration = AppBarConfiguration.Builder(bottomNavigationView.menu).build()
+
+        NavigationUI.setupActionBarWithNavController(this, controller, configuration)
         NavigationUI.setupWithNavController(bottomNavigationView, controller)
-
-
-//        val configuration = AppBarConfiguration(
-//            setOf(
-//                R.id.navigation_home,
-//                R.id.navigation_report,
-//                R.id.navigation_settings
-//            )
-//        )
-//
-//        NavigationUI.setupActionBarWithNavController(this, controller, configuration)
-//        bottomNavigationView.setupWithNavController(controller)
     }
 }
